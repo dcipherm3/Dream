1ec2c9c01c6373ef10fc3e7ff3388361b8484d7c
BlackMerchant <zafree0019@gmail.com>
2025-03-11 15:13:37 +0800

    Added video and fixed header

--- dream-react/src/components/VideoComponent.jsx
+++ dream-react/src/components/VideoComponent.jsx
@@ -3,8 +3,8 @@ import videoFile from "../assets/videos/dreambg.mp4";
 
 function VideoComponent() {
     return (
-      <video width="100%" height="100%" controls autoPlay loop>
-        <source src="{videoFile}" type="video/mp4" />
+      <video width="100%" height="100%" autoPlay loop muted>
+        <source src={videoFile} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     );
--- dream-react/src/components/header.jsx
+++ dream-react/src/components/header.jsx
@@ -3,8 +3,8 @@ import '../assets/styles/App.css';
 
 function Header(){
     return (
-        <header className="absolute top-0 left-0 p-6 w-full bg-blue-500 flex justify-between items-center">
-            <div className="header-title">
+        <header className="absolute top-0 left-0 pt-12 px-10 w-full bg-blue-500 flex justify-between items-center">
+            <div className="header-title pl-6">
                 <h1>Dream</h1>
             </div>
             <div className="header-items">
