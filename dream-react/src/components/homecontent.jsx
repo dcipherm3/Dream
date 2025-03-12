fb25ca6cb61b2a98399d0439ddefe3c0212bd2bf
PusakStar <hiqal2003@gmail.com>
2025-03-12 15:25:07 +0800

    added the registration form

--- dream-react/src/components/homecontent.jsx
+++ dream-react/src/components/homecontent.jsx
@@ -12,6 +12,28 @@ function Homecontent(){
                     <p>A software that touch human's dream</p>
                     <a href="#downloadsoftware">Make it happen</a>
                 </div>
+                <div className="form-container">
+                    <div className="formcontent__wrapper">
+                        <div className="register-form flex flex-col justify-center">
+                            <h1>Registration</h1>
+                            <div className="grid grid-cols-2">
+                                <input type="text" placeholder="Username" name="username" id="username" />
+                                <input type="text" placeholder="Email" name="email" id="email" />
+                            </div>
+                            <div className="grid grid-cols-1">
+                                <input type="password" placeholder="password" name="password" id="password" />
+                            </div>
+                            <div className="grid grid-cols-1">
+                                <input type="password" placeholder="retype-password" name="password" id="password" />
+                            </div>
+                            <div className="flex flex-col justify-center items-center">
+                                <button type="submit" name="regbtn" id="regbtn">Register</button>
+                                <span>Already have an account? <a href="#login">Login now</a></span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
 
         </section>
